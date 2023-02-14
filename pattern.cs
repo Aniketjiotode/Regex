b033a2771ec2bbc03e8796b5da4aec7619c93a9f
Aniketjiotode <aniketjiotode07@gmail.com>
2023-02-14 20:41:01 +0530

    Added: UC_5 Changes

--- pattern.cs
+++ pattern.cs
@@ -10,14 +10,15 @@ namespace Regex_
         public static string name = "^[A-Z][a-z]{2,9}$";
         public static string Fullname = "^[A-Z][a-z]{2,9}[ ][A-Z][a-z]{2,9}$";
         public static string Email = @"[a-z]{3,9}.?[a-z0-9]{3,8}[@][a-z]{3,8}[\.](com|org|in|net)$";
-        public static string PhoneNumber = @"^[0-9]{2}[ ][0-9]{10}$";
+        public static string PhoneNumber = @"^[0-9]{2}[ ][0-9]{1010}$";
+        public static string Password = @"^[a-zA-Z]{8,}$";
 
         public void patterncheck()
         {
-            Regex regex= new Regex(PhoneNumber);
-            Console.WriteLine("Enter the phoneNumber:");
-            string Number=Console.ReadLine();
-            Console.WriteLine(regex.IsMatch(Number));   
+            Regex regex= new Regex(Password);
+            Console.WriteLine("Enter the Password:");
+            string password=Console.ReadLine();
+            Console.WriteLine(regex.IsMatch(password));   
         }
     }
 }
