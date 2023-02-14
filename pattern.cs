aac42516bf7f37d4f7801f937564bba299414cc8
Aniketjiotode <aniketjiotode07@gmail.com>
2023-02-14 20:55:55 +0530

    Added: UC_6 Changes

--- pattern.cs
+++ pattern.cs
@@ -11,8 +11,7 @@ namespace Regex_
         public static string Fullname = "^[A-Z][a-z]{2,9}[ ][A-Z][a-z]{2,9}$";
         public static string Email = @"[a-z]{3,9}.?[a-z0-9]{3,8}[@][a-z]{3,8}[\.](com|org|in|net)$";
         public static string PhoneNumber = @"^[0-9]{2}[ ][0-9]{1010}$";
-        public static string Password = @"^[a-zA-Z]{8,}$";
-
+        public static string Password = @"^(?=.*?[A-Z])(?=.*?[a-z]).{8,}$";
         public void patterncheck()
         {
             Regex regex= new Regex(Password);
