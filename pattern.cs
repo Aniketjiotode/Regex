2970383c558e9c5d6ba20bda436ee358c25b78d5
Aniketjiotode <aniketjiotode07@gmail.com>
2023-02-14 21:13:30 +0530

    Added: UC_9 Changes

--- pattern.cs
+++ pattern.cs
@@ -10,14 +10,26 @@ namespace Regex_
         public static string name = "^[A-Z][a-z]{2,9}$";
         public static string Fullname = "^[A-Z][a-z]{2,9}[ ][A-Z][a-z]{2,9}$";
         public static string Email = @"[a-z]{3,9}.?[a-z0-9]{3,8}[@][a-z]{3,8}[\.](com|org|in|net)$";
-        public static string PhoneNumber = @"^[0-9]{2}[ ][0-9]{1010}$";
+        public static string PhoneNumber = @"^[0-9]{2}[ ][0-9]{10,}$";
         public static string Password = @"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$";
         public void patterncheck()
         {
-            Regex regex= new Regex(Password);
+            Regex r1= new Regex(Fullname);
+            Console.WriteLine("Enter the FullName:");
+            string Fname=Console.ReadLine();
+            Console.WriteLine(r1.IsMatch(Fname));   
+            Regex r2= new Regex(Email);
+            Console.WriteLine("Enter the EmailId:");
+            string email=Console.ReadLine();
+            Console.WriteLine(r2.IsMatch(email)); 
+            Regex r3= new Regex(PhoneNumber);
+            Console.WriteLine("Enter the PhoneNumber:");
+            string number=Console.ReadLine();
+            Console.WriteLine(r3.IsMatch(number)); 
+            Regex r4= new Regex(Password);
             Console.WriteLine("Enter the Password:");
             string password=Console.ReadLine();
-            Console.WriteLine(regex.IsMatch(password));   
+            Console.WriteLine(r4.IsMatch(password));   
         }
     }
 }
