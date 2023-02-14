eac11a88cf0ecc1591260ec6b2a1695d0f7ee856
Aniketjiotode <aniketjiotode07@gmail.com>
2023-02-14 20:06:00 +0530

    Added: UC_3 Changes

--- pattern.cs
+++ pattern.cs
@@ -9,13 +9,14 @@ namespace Regex_
     {
         public static string name = "^[A-Z][a-z]{2,9}$";
         public static string Fullname = "^[A-Z][a-z]{2,9}[ ][A-Z][a-z]{2,9}$";
-         
+        public static string Email = @"[a-z]{3,7}.?[a-z0-9]{3,8}[@][a-z]{3,8}.(com|org)$";
+
         public void patterncheck()
         {
-            Regex regex= new Regex(Fullname);
-            Console.WriteLine("Enter the Fullname:");
-            string FName=Console.ReadLine();
-            Console.WriteLine(regex.IsMatch(FName));    
+            Regex regex= new Regex(Email);
+            Console.WriteLine("Enter the EmailId:");
+            string email=Console.ReadLine();
+            Console.WriteLine(regex.IsMatch(email));   
         }
     }
 }
