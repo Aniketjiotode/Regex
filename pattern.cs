a6332fc300e65296a769dd2f316ac89a2377bca9
Aniketjiotode <aniketjiotode07@gmail.com>
2023-02-14 18:30:47 +0530

    Added: UC_2 Changes

--- pattern.cs
+++ pattern.cs
@@ -8,14 +8,14 @@ namespace Regex_
     internal class pattern
     {
         public static string name = "^[A-Z][a-z]{2,9}$";
-        public static string Lastname = "^[A-Z][a-z]{2,9}$";
+        public static string Fullname = "^[A-Z][a-z]{2,9}[ ][A-Z][a-z]{2,9}$";
          
         public void patterncheck()
         {
-            Regex regex= new Regex(Lastname);
-            Console.WriteLine("Enter the name:");
-            string LName=Console.ReadLine();
-            Console.WriteLine(regex.IsMatch(LName));    
+            Regex regex= new Regex(Fullname);
+            Console.WriteLine("Enter the Fullname:");
+            string FName=Console.ReadLine();
+            Console.WriteLine(regex.IsMatch(FName));    
         }
     }
 }
