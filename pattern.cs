8f0c0f32e0da8ee9bdbfee9b64b98e3b13f1e3d1
Aniketjiotode <aniketjiotode07@gmail.com>
2023-02-14 20:12:02 +0530

    Added: UC_4 Changes

--- pattern.cs
+++ pattern.cs
@@ -9,14 +9,15 @@ namespace Regex_
     {
         public static string name = "^[A-Z][a-z]{2,9}$";
         public static string Fullname = "^[A-Z][a-z]{2,9}[ ][A-Z][a-z]{2,9}$";
-        public static string Email = @"[a-z]{3,7}.?[a-z0-9]{3,8}[@][a-z]{3,8}.(com|org)$";
+        public static string Email = @"[a-z]{3,9}.?[a-z0-9]{3,8}[@][a-z]{3,8}[\.](com|org|in|net)$";
+        public static string PhoneNumber = @"^[0-9]{2}[ ][0-9]{10}$";
 
         public void patterncheck()
         {
-            Regex regex= new Regex(Email);
-            Console.WriteLine("Enter the EmailId:");
-            string email=Console.ReadLine();
-            Console.WriteLine(regex.IsMatch(email));   
+            Regex regex= new Regex(PhoneNumber);
+            Console.WriteLine("Enter the phoneNumber:");
+            string Number=Console.ReadLine();
+            Console.WriteLine(regex.IsMatch(Number));   
         }
     }
 }
